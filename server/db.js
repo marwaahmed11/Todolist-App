d7cb55c1777ac5c5321719346e06fdecde27caf2
marwaahmed11 <marwaahmed200126@gmail.com>
2026-08-11 00:13:25 +0300

    first commit

--- server/db.js
+++ server/db.js
@@ -12,11 +12,10 @@ module.exports = async () => {
             //mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.3.2
 
             // "mongodb://mongodb:mongodb@"+process.env.ENV_PORT+":27017/mongodb?directConnection=true&serverSelectionTimeoutMS=2000",
-            //   "mongodb://127.0.0.1:27017/todo-app",
-            
+               "mongodb://127.0.0.1:27017/todo-app",  // to run mongodb in local machine
             //"mongodb://<db-container-name if they are in the same network>:27017/todo-app",
             //"mongodb://mongodb:27017/todo-app",
-            "mongodb://mongodb:27017/todo-app",
+           
               connectionParams
           );
         console.log("Connected to MongoDB successfully.");
